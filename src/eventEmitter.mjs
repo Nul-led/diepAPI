d33f9c384027869feb7f10066979f3fd25b1baa9
Nulled <63370961+Nul-led@users.noreply.github.com>
2026-08-10 12:16:11 +0200

    Fix EventEmitter listener removal

--- src/eventEmitter.mjs
+++ src/eventEmitter.mjs
@@ -1,4 +1,9 @@
 export default class EventEmitter extends EventTarget {
+    constructor() {
+        super();
+        this._listenerWrappers = new Map();
+    }
+
     /**
      *
      * @param {string} eventName The name of the event
@@ -14,7 +19,8 @@ export default class EventEmitter extends EventTarget {
      * @param {Function} listener The callback function
      */
     on(eventName, listener) {
-        this.addEventListener(eventName, (e) => Reflect.apply(listener, this, e.detail));
+        const wrapped = this._wrapListener(eventName, listener);
+        this.addEventListener(eventName, wrapped);
     }
 
     /**
@@ -23,7 +29,8 @@ export default class EventEmitter extends EventTarget {
      * @param {Function} listener The callback function
      */
     once(eventName, listener) {
-        this.addEventListener(eventName, (e) => Reflect.apply(listener, this, e.detail), { once: true });
+        const wrapped = this._wrapListener(eventName, listener, true);
+        this.addEventListener(eventName, wrapped, { once: true });
     }
 
     /**
@@ -32,6 +39,50 @@ export default class EventEmitter extends EventTarget {
      * @param {Function} listener The callback function
      */
     off(eventName, listener) {
-        this.removeEventListener(eventName, listener);
+        const eventListeners = this._listenerWrappers.get(eventName);
+        const wrappers = eventListeners?.get(listener);
+        const wrapped = wrappers?.pop();
+        if (!wrapped) return;
+
+        this.removeEventListener(eventName, wrapped);
+        this._cleanupListenerEntry(eventName, listener, eventListeners, wrappers);
+    }
+
+    _wrapListener(eventName, listener, once = false) {
+        const wrapped = (event) => {
+            if (once) this._forgetWrapper(eventName, listener, wrapped);
+            Reflect.apply(listener, this, event.detail);
+        };
+
+        let eventListeners = this._listenerWrappers.get(eventName);
+        if (!eventListeners) {
+            eventListeners = new Map();
+            this._listenerWrappers.set(eventName, eventListeners);
+        }
+
+        let wrappers = eventListeners.get(listener);
+        if (!wrappers) {
+            wrappers = [];
+            eventListeners.set(listener, wrappers);
+        }
+        wrappers.push(wrapped);
+        return wrapped;
+    }
+
+    _forgetWrapper(eventName, listener, wrapped) {
+        const eventListeners = this._listenerWrappers.get(eventName);
+        const wrappers = eventListeners?.get(listener);
+        if (!wrappers) return;
+
+        const index = wrappers.indexOf(wrapped);
+        if (index !== -1) wrappers.splice(index, 1);
+        this._cleanupListenerEntry(eventName, listener, eventListeners, wrappers);
+    }
+
+    _cleanupListenerEntry(eventName, listener, eventListeners, wrappers) {
+        if (wrappers.length > 0) return;
+
+        eventListeners.delete(listener);
+        if (eventListeners.size === 0) this._listenerWrappers.delete(eventName);
     }
 }
